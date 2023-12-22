a7d04fc1918a993a932f95be312ebcb4580f24f2
joao-andreotti <jvmandreotti@hotmail.com>
2023-12-22 13:23:45 +0100

    update gender visualisation type

--- frontend/src/components/NobelsByGender.js
+++ frontend/src/components/NobelsByGender.js
@@ -1,5 +1,6 @@
 import { NOBEL_CATEGORIES } from "../App";
-import { ResponsiveSwarmPlot } from '@nivo/swarmplot'
+import { ResponsiveWaffle } from '@nivo/waffle'
+// import { ResponsiveSwarmPlot } from '@nivo/swarmplot'
 import { useMemo, useState } from "react";
 import "./NobelsByGender.css"
 
@@ -30,59 +31,91 @@ export const NobelsByGender = ({ completeData }) => {
         Object.fromEntries(NOBEL_CATEGORIES.map(category => [category, true]))
     );
     const chartData = getChartData(completeData)
+    
+    const filteredData = useMemo(() => {
+            return chartData
+                .filter(({ category }) => selectedCategories[category])
+        },
+        [selectedCategories, chartData]
+    );
 
-    const filteredData = useMemo(() => chartData.filter(({ category }) => selectedCategories[category]),
-        [selectedCategories, chartData]);
+    const aggregatedData = useMemo(() => {
+        return filteredData.reduce((acc, { group, category }) => {
+            if (!acc.length) {
+                acc = [
+                    {
+                        id: "female",
+                        value: 0,
+                        label: "Female",
+                    },
+                    {
+                        id: "male",
+                        value: 0,
+                        label: "Male",
+                    },
+                ]
+            }
+
+            if (group === "male")
+                acc[1].value += 1
+            else
+                acc[0].value += 1
+
+            return acc
+        }, [])
+    }, [filteredData]);
+
+    console.log(aggregatedData)
     return (
         <div className="outer-container">
             <SelectCategory selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories} />
             <div className="chart-container">
-                <ResponsiveSwarmPlot
-                    data={filteredData}
-                    groups={['male', 'female']}
-                    identity="id"
-                    height={500}
-                    width={800}
-                    value="year"
-                    valueScale={{ type: 'linear', min: 1898, max: 2025, reverse: false }}
-                    forceStrength={2}
-                    simulationIterations={80}
-                    colorBy={(
-                        node
-                    ) =>
-                        node.data.category}
-                    margin={{ top: 50, right: 100, bottom: 80, left: 100 }}
-                    axisBottom={{
-                        orient: 'bottom',
-                        tickSize: 10,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'Gender',
-                        legendPosition: 'middle',
-                        legendOffset: 46
+                <ResponsiveWaffle
+                    data={aggregatedData}
+                    total={aggregatedData.reduce((acc, { value }) => acc + value, 0)}
+                    rows={aggregatedData.reduce((acc, { value }) => acc + value, 0) / 25}
+                    columns={25}
+                    padding={1}
+                    valueFormat=".2f"
+                    margin={{ top: 10, right: 10, bottom: 10, left: 120 }}
+                    colors={{ scheme: 'nivo' }}
+                    borderRadius={3}
+                    borderColor={{
+                        from: 'color',
+                        modifiers: [
+                            [
+                                'darker',
+                                0.3
+                            ]
+                        ]
                     }}
-                    tooltip={(
-                        node
-                    ) => <div style={{
-                        color: node.color,
-                        background: '#333',
-                        padding: '12px 16px'
-                    }}>
-                            <strong>
-                                {node.data.name}
-                                <br />
-                                {node.data.category} - {node.data.year}
-                            </strong>
-                        </div>}
-                    axisLeft={{
-                        orient: 'left',
-                        tickSize: 10,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'Year of the award',
-                        legendPosition: 'middle',
-                        legendOffset: -76
-                    }}
+                    emptyOpacity={0}
+                    motionStagger={2}
+                    legends={[
+                        {
+                            anchor: 'top-left',
+                            direction: 'column',
+                            justify: false,
+                            translateX: -100,
+                            translateY: 0,
+                            itemsSpacing: 4,
+                            itemWidth: 100,
+                            itemHeight: 20,
+                            itemDirection: 'left-to-right',
+                            itemOpacity: 1,
+                            itemTextColor: '#000000',
+                            symbolSize: 20,
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemTextColor: '#000',
+                                        itemBackground: '#f7fafb'
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
                 />
             </div>
         </div>
